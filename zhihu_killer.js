c38085b206e2d06756842662e26df07c344e4c1b
wuuconix <1521900139@qq.com>
2022-04-09 23:24:23 +0800

    修改zhihu_killer.js

--- zhihu_killer.js
+++ zhihu_killer.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  知乎自动关闭登录框 智能识别登录状态
 // @author       wuuconix
-// @match        https://zhuanlan.zhihu.com/*
+// @match        https://*.zhihu.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -15,10 +15,10 @@
     const observer = new MutationObserver(() => {
         let avaterButton = document.querySelector(".Button.AppHeader-profileEntry.Button--plain")
         let closeButton = document.querySelector(".Button.Modal-closeButton.Button--plain")
-        if (avaterButton) {
+        if (avaterButton) { //用户已经登录
             console.log("检测到你已登录，observer将自动关闭")
-            suicide() //自杀，已经不用再观察了
-        } else if (closeButton) {
+            suicide()
+        } else if (closeButton) { //检查到弹出框
             closeButton.click()
             console.log("检测到登录框 已经为你自动关闭")
             suicide()
@@ -30,5 +30,4 @@
     const suicide = () => {
         observer.disconnect()
     }
-
 })();
