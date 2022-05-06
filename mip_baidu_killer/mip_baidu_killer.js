b621a290e306f4fd02eaddd9ccd9c266ded72e2e
wuuconix <1521900139@qq.com>
2022-05-06 15:09:51 +0800

    mip zhihu uooconlien 对MutationObserver进行优雅关闭 升级到0.2版本

--- mip_baidu_killer/mip_baidu_killer.js
+++ mip_baidu_killer/mip_baidu_killer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mip_baidu_killer.js
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  IP地址查询-站长查询 百度网盟广告自动删除
 // @author       wuuconix
 // @match        http://mip.chinaz.com/*
@@ -13,18 +13,18 @@
 (function() {
     'use strict'
 
+    let number = 0
     const observer = new MutationObserver(() => {
         let baiduFrame = document.querySelector("iframe")
         if (baiduFrame) { //发现百度网盟推广广告
             baiduFrame.parentElement.remove()
             console.log("百度网盟广告已经自动关闭")
-            suicide()
+            number++
+            if (number == 2) { //广告会出现两次，两次全部关闭后即可退出监视
+                observer.disconnect()
+            }
         }
     });
 
     observer.observe(document.body, { childList: true, });
-
-    const suicide = () => {
-        observer.disconnect()
-    }
 })();
--- zhihu_killer/zhihu_killer.js
+++ zhihu_killer/zhihu_killer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         zhihu_killer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  知乎自动关闭登录框 智能识别登录状态
 // @author       wuuconix
 // @match        https://*.zhihu.com/*
@@ -18,17 +18,13 @@
         let closeButton = document.querySelector(".Button.Modal-closeButton.Button--plain")
         if (avaterButton) { //用户已经登录
             console.log("检测到你已登录，observer将自动关闭")
-            suicide()
+            observer.disconnect()
         } else if (closeButton) { //检查到弹出框
             closeButton.click()
             console.log("检测到登录框 已经为你自动关闭")
-            suicide()
+            observer.disconnect()
         }
     });
 
     observer.observe(document.body, { childList: true, });
-
-    const suicide = () => {
-        observer.disconnect()
-    }
 })();
