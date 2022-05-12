d2ee7ca05d332d8399141b1796ea1e8000718d1a
wuuconix <1521900139@qq.com>
2022-05-12 15:05:26 +0800

    更新uooconline_killer.js 至0.2，增加凌晨自动签到的功能

--- uooconline_killer/README.md
+++ uooconline_killer/README.md
@@ -1,12 +1,22 @@
-## 优选在线 视频暂停杀手
+## 优选在线 暂停 + 签到杀手
 
 > [uooconline_killer.js](https://github.com/wuuconix/scripts/blob/main/uooconline_killer/uooconline_killer.js)
 
-解决问题：[优课在线](http://www.uooconline.com/)的网课在鼠标离开视频后会自动暂停，影响刷课效率。
+解决问题：
 
-该脚本通过监听video的pause事件，执行video.play()从而重新播放视频，让用户可以正常得把视频挂在后台。
+1. [优课在线](http://www.uooconline.com/)的网课在鼠标离开视频后会自动暂停，影响刷课效率。
 
-![重新播放](https://tvax3.sinaimg.cn/large/007YVyKcly1h1xeiy7vj2j31k50r215k.jpg)
+    该脚本通过监听video的pause事件，执行video.play()从而重新播放视频，让用户可以正常得把视频挂在后台。
+
+    ![重新播放](https://tvax3.sinaimg.cn/large/007YVyKcly1h1xeiy7vj2j31k50r215k.jpg)
+
+2. 优课在线的课程居然有签到要求，每天需要点击课程主页面的**开始学习**按钮才算一次签到。
+
+    所以此脚本还支持了凌晨自动点击开始学习按钮，实现签到。
+
+    ![签到](https://tva4.sinaimg.cn/large/007YVyKcly1h25mtfkcbfj30n308j0w8.jpg)
+
+    > 当然此功能只适用那些 有一台二十四小时不关机的服务器的同学们。
 
 目前支持网站：**www.uooconline.com**
 
--- uooconline_killer/uooconline_killer.js
+++ uooconline_killer/uooconline_killer.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         uooconline_killer.js
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  优课在线的视频鼠标离开视频后会自动暂停 该脚本则监听到暂停事件后自动播放
+// @version      0.2
+// @description  优课在线免受暂停的干扰 + 每天凌晨自动进入学习页面实现签到
 // @author       wuuconix
 // @match        http://www.uooconline.com/*
 // @icon         http://www.uooconline.com/favicon.ico
@@ -14,6 +14,8 @@
     let video, start
     const observer = new MutationObserver(() => { //页面变化监听器
         let temp = document.querySelector("button[title='Play Video']")
+        let backButton = document.querySelector("a.goback.fl_right.ng-scope")
+        let enterButton = document.querySelector("a.btn.btn-danger.ng-binding.ng-scope")
         if (temp !== null && temp != start) { //开始按钮变化 说明页面改变了
             start = temp
             start.click() //自动点击播放按钮
@@ -23,6 +25,20 @@
                     .then(() => console.log("视频重新播放成功"))
                     .catch(err => console.log(`视频重新播放失败: ${err}`))
             })
+            video.addEventListener("ended", () => {
+                backButton.click()
+            })
+        }
+        if (enterButton) {
+            let honor = new Date().getHours()
+            let minute = new Date().getMinutes()
+            let waitMin = 24 * 60 + 30 - (honor * 60 + minute)
+            console.log(`现在的时间是: ${new Date()}`)
+            console.log(`将会在24点30分，${waitMin}分钟后签到`)
+            setTimeout(() => {
+                enterButton.click()
+            }, waitMin * 60 * 1000)
+            observer.disconnect()
         }
     });
     observer.observe(document.body, { childList: true, subtree: true });
