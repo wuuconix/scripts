b442c2223929d71ed7932a536e48e07a312ef5a2
wuuconix <1521900139@qq.com>
2022-12-26 14:17:16 +0800

    update ddys_ad to 0.2, change domain

--- ddys_ad/ddys_ad.js
+++ ddys_ad/ddys_ad.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         ddys_ad.js
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  低端影视广告隐藏
 // @author       wuuconix
-// @match        https://ddys.tv/*
+// @match        https://ddys.art/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ddys.tv
 // @grant        none
 // @license      MIT
