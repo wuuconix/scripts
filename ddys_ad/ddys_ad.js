72df52b6a180c6bdf617ad7b2286b9c097e589aa
wuuconix <wuuconix@qq.com>
2023-01-22 20:24:31 +0800

    ddys_ad add api.goodjson.top

--- ddys_ad/ddys_ad.js
+++ ddys_ad/ddys_ad.js
@@ -6,6 +6,7 @@
 // @author       wuuconix
 // @match        https://ddys.art/*
 // @match        https://www.fantuanhd.com/*
+// @match        https://api.goodjson.top/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ddys.tv
 // @grant        none
 // @license      MIT
@@ -14,7 +15,8 @@
 const style = document.createElement("style")
 const map = {
   "ddys.art": ["#sajdhfbjwhe", "#iaujwnefhw", "#fkasjgf"],
-  "www.fantuanhd.com": ["div#adv_wrap_hh", "#t-img-box", "#HMRichBox"]
+  "www.fantuanhd.com": ["#t-img-box", "#HMRichBox"],
+  "api.goodjson.top": ["#adv_wrap_hh"]
 }
 style.innerHTML = `${map[window.location.host].join(",")} {
   display: none !important;
