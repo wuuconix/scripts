6fbe5c5f3edb5dd3c497bf5588f4cace6c2f7be4
wuuconix <wuuconix@qq.com>
2023-01-22 16:37:10 +0800

    ddys_ad support fantuanhd.com

--- ddys_ad/ddys_ad.js
+++ ddys_ad/ddys_ad.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         ddys_ad.js
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  低端影视广告隐藏
+// @version      0.3
+// @description  广告隐藏
 // @author       wuuconix
 // @match        https://ddys.art/*
+// @match        https://www.fantuanhd.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ddys.tv
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
 const style = document.createElement("style")
-style.innerHTML = `#sajdhfbjwhe, #iaujwnefhw, #fkasjgf {
-    display: none;
+const map = {
+  "ddys.art": ["#sajdhfbjwhe", "#iaujwnefhw", "#fkasjgf"],
+  "www.fantuanhd.com": ["div#adv_wrap_hh", "#t-img-box", "#HMRichBox"]
+}
+style.innerHTML = `${map[window.location.host].join(",")} {
+  display: none !important;
 }`
 document.head.appendChild(style)
