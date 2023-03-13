a8a9eaf7ed92f3706b35c96c4e5657ba41f397a5
wuuconix <wuuconix@qq.com>
2023-03-13 16:24:47 +0800

    update paste_free.js using delegation(best practice)

--- paste_free/paste_free.js
+++ paste_free/paste_free.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         paste_free
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  点击图标 即可自动读取剪切板中的内容进行搜索
 // @author       wuuconix
 // @match        https://fofa.info/result*
@@ -11,51 +11,50 @@
 // @grant        none
 // ==/UserScript==
 
-if (location.hostname == "fofa.info") { //fofa
-setTimeout(() => {
-  const fofa = document.querySelector("div.headerLeft")
-  fofa.addEventListener("click", async (e) => {
-    e.preventDefault()
-    const text = await navigator.clipboard.readText()
-    let base64 = ""
-    if (/^(\d{1,3}.){3}\d{1,3}$/.test(text)) {
-        base64 = btoa(`ip="${text}" && status_code="200"`)
-        location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
-    } else {
-        base64 = btoa(`domain="${text}" && status_code="200"`)
-        location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
+if (location.hostname == "fofa.info") {                     // fofa
+  document.body.addEventListener("click", async (e) => {
+    if (e.target.className == "fofa-logo") {
+      e.preventDefault()
+      const text = await navigator.clipboard.readText()
+      let base64 = ""
+      if (/^(\d{1,3}.){3}\d{1,3}$/.test(text)) {
+          base64 = btoa(`ip="${text}" && status_code="200"`)
+          location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
+      } else {
+          base64 = btoa(`domain="${text}" && status_code="200"`)
+          location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
+      }
     }
   })
-  fofa.addEventListener("contextmenu", async (e) => {
-    e.preventDefault()
-    const text = await navigator.clipboard.readText()
-    let base64 = ""
-    if (/^(\d{1,3}.){3}\d{1,3}$/.test(text)) {
-      base64 = btoa(`ip="${text}"`)
-      location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
-    } else {
-      base64 = btoa(`domain="${text}"`)
-      location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
+  document.body.addEventListener("contextmenu", async (e) => {
+    if (e.target.className == "fofa-logo") {
+      e.preventDefault()
+      const text = await navigator.clipboard.readText()
+      let base64 = ""
+      if (/^(\d{1,3}.){3}\d{1,3}$/.test(text)) {
+        base64 = btoa(`ip="${text}"`)
+        location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
+      } else {
+        base64 = btoa(`domain="${text}"`)
+        location.replace(`${location.origin}${location.pathname}?qbase64=${base64}`)
+      }
     }
   })
-}, 1000)
-} else if (/bing.com/.test(location.hostname)) { //bing
-    setTimeout(() => {
-      const bing = document.querySelector("#sb_form > a")
-      bing.addEventListener("click", async (e) => {
-        e.preventDefault()
-        const text = await navigator.clipboard.readText()
-        const q = `site:${text}`
-        location.replace(`${location.origin}${location.pathname}?q=${q}`)
-      })
-    }, 2000)
-    setTimeout(() => {
-      const bing = document.querySelector("#sb_form > a")
-      bing.addEventListener("contextmenu", async (e) => {
-        e.preventDefault()
-        const text = await navigator.clipboard.readText()
-        const q = encodeURIComponent(`site:${text} & filetype: (pdf | doc | xls | ppt | csv)`)
-        location.replace(`${location.origin}${location.pathname}?q=${q}`)
-      })
-  }, 2000)
+} else if (/bing.com/.test(location.hostname)) {            // bing
+  document.body.addEventListener("click", async (e) => {
+    if (e.target.className == "b_logo") {
+      e.preventDefault()
+      const text = await navigator.clipboard.readText()
+      const q = `site:${text}`
+      location.replace(`${location.origin}${location.pathname}?q=${q}`)
+    }
+  })
+  document.body.addEventListener("contextmenu", async (e) => {
+    if (e.target.className == "b_logo") {
+      e.preventDefault()
+      const text = await navigator.clipboard.readText()
+      const q = encodeURIComponent(`site:${text} & filetype: (pdf | doc | xls | ppt | csv)`)
+      location.replace(`${location.origin}${location.pathname}?q=${q}`)
+    }
+  })
 }
