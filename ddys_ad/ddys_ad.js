6acb04d3095010b642ec0a7c027adce16f3ac924
wuuconix <wuuconix@qq.com>
2023-08-17 22:02:36 +0800

    update ddys_add to 0.7

--- ddys_ad/ddys_ad.js
+++ ddys_ad/ddys_ad.js
@@ -1,28 +1,23 @@
 // ==UserScript==
 // @name         ddys_ad.js
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  广告隐藏
 // @author       wuuconix
-// @match        https://ddys.art/*
-// @match        https://www.fantuanhd.com/*
-// @match        https://api.goodjson.top/*
-// @match        http://www.yinghuacd.com/*
-// @match        https://tup.yinghuacd.com/*
+// @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ddys.tv
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
+let adList = localStorage.getItem("ddys_ad")
 const style = document.createElement("style")
-const map = {
-  "ddys.art": ["#sajdhfbjwhe", "#iaujwnefhw", "#fkasjgf"],
-  "www.fantuanhd.com": ["#t-img-box", "#HMRichBox", "#note", "div.stui-pannel__bd div:nth-child(2)"],
-  "api.goodjson.top": ["#adv_wrap_hh"],
-  "tup.yinghuacd.com": ["#adv_wrap_hh"],
-  "www.yinghuacd.com": ["#HMimageleft", "#HMimageright", "#HMRichBox"]
-}
-style.innerHTML = `${map[window.location.host].join(",")} {
-  display: none !important;
+
+if (adList) {
+    adList = JSON.parse(adList)
+    style.innerHTML = `${adList.join(",")} {
+  width: 0px;
+  height: 0px;
 }`
-document.head.appendChild(style)
+    document.head.appendChild(style)
+}
